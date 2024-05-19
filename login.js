e5ef03f7fe77b9d05874b0e2c5c05ba4ddac8a88
zxcv <56207419+onurcatalkaya@users.noreply.github.com>
2024-05-19 23:37:25 +0300

    Update login.js

--- login.js
+++ login.js
@@ -4,17 +4,28 @@ document.getElementById("signInButton").addEventListener("click", function() {
     var errorMessage = document.getElementById("errorMessage");
     
     if (username === "bahcelievler" && password === "bahcelievler") {
-        // Kullanıcıyı istediğiniz sayfaya yönlendirme
-        window.location.href = "main.html";
+        window.location.href = "main1.html";
     }
     else if (username === "suphan" && password === "suphan"){
-        window.location.href = "main.html";
+        window.location.href = "main1.html";
     }
     else if (username === "kemalsunal" && password === "kemalsunal"){
-        window.location.href = "main.html";
+        window.location.href = "main1.html";
     }
-    else if (username === "1" && password === "1"){
-        window.location.href = "main.html";
+    else if (username === "bahcelievler" && password === "bahcelievler"){
+        window.location.href = "main1.html";
+    }
+    else if (username === "stadyum" && password === "stadyum"){
+        window.location.href = "main1.html";
+    }
+    else if (username === "ahmetyesevi" && password === "ahmetyesevi"){
+        window.location.href = "main1.html";
+    }
+    else if (username === "önder" && password === "önder"){
+        window.location.href = "main1.html";
+    }
+    else if (username === "onur" && password === "onur"){
+        window.location.href = "main1.html";
     }
     else {
         // Başarısız giriş bildirimi
